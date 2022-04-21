d2f8b844e2824cbc2958d8b590f309ec649bb9ae
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-04-21 15:47:07 +0300

    bug fixed

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,12 +1,12 @@
 // Database bilgi ekleme,bilgiyi alma, bilgi silme ve değiştirme
-import { firebase } from "./firebase"
+import firebase from "./firebase"
 import {useState,useEffect} from "react";
 import { getDatabase,ref,set,push,onValue, remove,update} from "firebase/database";
 import Toastify from "./toast";
 
 // Bilgi Ekleme
 export const AddUser=(info)=>{
-    const db = getDatabase();
+    const db = getDatabase(firebase);
     const userRef=ref(db,"baglanti");
     const newUserRef=push(userRef);
     set((newUserRef),{
@@ -25,7 +25,7 @@ export const useFetch=()=>{
     useEffect(() => {
         setIsLoading(true)
 
-        const db = getDatabase();
+        const db = getDatabase(firebase);
         const userRef=ref(db,"baglanti");
 
         onValue(userRef, (snapshot) => {
@@ -44,7 +44,7 @@ export const useFetch=()=>{
 
 // Bilgi silme
 export const DeleteUser=(id)=>{
-        const db = getDatabase();
+        const db = getDatabase(firebase);
         //c  onst userRef=ref(db,"baglanti");
         remove(ref(db,"baglanti/"+id));
         Toastify("Kullanıcı bilgisi silindi")
@@ -53,7 +53,7 @@ export const DeleteUser=(id)=>{
 // Bilgi Değiştirme
 
 export const EditUser=(info)=>{
-    const db = getDatabase();
+    const db = getDatabase(firebase);
     const updates = {};
 
     updates["baglanti/"+info.id]=info;
