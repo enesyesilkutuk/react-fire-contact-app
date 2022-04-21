f8f221176f7fb59bded160a33e6d962709577f16
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-04-21 15:13:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+ import './App.css';
+import {useState} from "react";
+import FormComponent from './components/form/FormComponent';
+import Contacts from "./components/contacts/Contacts"
+import { AddUser,EditUser } from './utils/functions';
+import { ToastContainer } from "react-toastify";
+
+const initialValues={username:"",phoneNumber:"",gender:""}
+
+function App() {
+ const [info,setInfo]=useState(initialValues);
+
+ const handleFormSubmit=(e)=>{
+   e.preventDefault();
+  if (info.id) {
+    EditUser(info)
+  }
+   else {
+     AddUser(info)
+    }
+    setTimeout(() => {
+      setInfo(initialValues);
+    }, 1000);
+    
+ }
+
+ const editHandler=(id,username,phoneNumber,gender)=>{
+   setInfo({id,username,phoneNumber,gender})
+ }
+
+  return (
+    <div className="App">
+      <FormComponent info={info} setInfo={setInfo} handleFormSubmit={handleFormSubmit}/>
+      <Contacts editHandler={editHandler}/>
+      <ToastContainer/>
+    </div>
+  );
+}
+
+export default App;
